b948b5f792ab36982ddb361bf0ff14ada73e79d4
denisgodoy <denisgodoy1@gmail.com>
2021-03-06 14:44:37 -0300

    create routes

--- index.ts
+++ index.ts
@@ -1,4 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { pokemonArray } from './pokemonApi';
 const app = express();
+
+app.get('/', (req: Request, res: Response) => res.redirect('/pokemon'));
+
+app.get('/pokemon', (req: Request, res: Response) => {
+    const { name } = req.query;
+    let queryPokemon: string =  pokemonArray.filter(q => q.includes(name || ''));
+    return res.send(queryPokemon);
+});
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
