3607b5e5a08f74d688096b39b787a98c61db7ce0
denisgodoy <denisgodoy1@gmail.com>
2021-03-06 15:51:36 -0300

    call next property to fetch more Pokémon

--- pokemonApi.ts
+++ pokemonApi.ts
@@ -7,12 +7,13 @@ const req = axios.create({
 let pokemonArray: any = [];
 
 function getPokemon(response: any){
-   let { results } = response.data;
+   let { results, next } = response.data;
    for(let pokemon of results){
        pokemonArray.push(pokemon.name);
     };
+    axios.get(next).then(getPokemon);
 };
 
-req.get('https://pokeapi.co/api/v2/pokemon?limit=1118').then(getPokemon);
+req.get('https://pokeapi.co/api/v2/pokemon').then(getPokemon);
 
 export { pokemonArray };
